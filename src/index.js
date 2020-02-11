9ec98071a95e2e9d15d97201876d148c54eff773
rudygzl <rudy.gonzalez@laposte.net>
2020-02-11 12:08:47 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import $ from 'jquery';
 import 'bootstrap';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import 'scrollmagic';
+
+$(document).ready(function() {
+        // init controller
+        var controller = new ScrollMagic.Controller();
+        // create a scene
+        var scene1 = new ScrollMagic.Scene({
+          triggerElement: '#scene1',
+          duration: 500,
+          offset: 300
+        })
+          .setClassToggle('#scene1', 'changeBackground')
+          .addTo(controller)
+});
